a2fb644a30b855d093370e59bd7bac3157dbbf81
JoyLight <134897775+JoyLight00@users.noreply.github.com>
2025-04-26 12:07:08 +0000

    done

--- models/index.js
+++ models/index.js
@@ -1,23 +1,7 @@
 const sequelize = require('../config/db.config');
-
-// Import all models
 const Wallet = require('./wallet.model');
-const HelpRequest = require('./helpRequest.model');
-const Project = require('./project.model');
-const User = require('./user.model');
-const SupportTicket = require('./supportTicket.model');
-const ValidatorRanking = require('./validatorRanking.model');
 
-// Optionally define associations (if any)
-const initModels = () => {
-  if (Wallet.associate) Wallet.associate({ Project, User, SupportTicket, ValidatorRanking });
-  if (Project.associate) Project.associate({ Wallet, User, SupportTicket, ValidatorRanking });
-  if (User.associate) User.associate({ Wallet, Project, SupportTicket, ValidatorRanking });
-  if (SupportTicket.associate) SupportTicket.associate({ Wallet, Project, User, ValidatorRanking });
-  if (ValidatorRanking.associate) ValidatorRanking.associate({ User });
-};
-
-// Test database connection and sync models
+// Test database connection
 const dbConnection = async () => {
   try {
     await sequelize.authenticate();
