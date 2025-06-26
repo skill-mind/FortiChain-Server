73489dd8dc19cf72c3158550a180a62302684b50
shamoo53 <aliyushamsu16@gmail.com>
2025-06-26 20:20:35 +0000

    corrections

--- src/error.rs
+++ src/error.rs
@@ -1,102 +1,75 @@
-use axum::{http::{StatusCode, HeaderMap, header}, response::{IntoResponse, Response}, Json};
-use serde::Serialize;
+use axum::http::header::WWW_AUTHENTICATE;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use std::borrow::Cow;
 use std::collections::HashMap;
 use thiserror::Error;
 
+/// A common error type that can be used throughout the API.
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Unauthorized")]
+    /// Return `401 Unauthorized`
+    #[error("authentication required")]
     Unauthorized,
-    #[error("Forbidden")]
+
+    /// Return `403 Forbidden`
+    #[error("user may not perform that action")]
     Forbidden,
-    #[error("Not Found")]
+
+    /// Return `404 Not Found`
+    #[error("request path not found")]
     NotFound,
-    #[error("Unprocessable Entity")] 
-    UnprocessableEntity(Errors),
-    #[error(transparent)]
-    Sqlx(#[from] sqlx::Error),
-    #[error(transparent)]
-    Anyhow(#[from] anyhow::Error),
-}
 
-#[derive(Serialize, Debug, Default)]
-pub struct Errors {
-    pub errors: HashMap<String, Vec<String>>,
-}
-
-impl Errors {
-    pub fn new() -> Self {
-        Self { errors: HashMap::new() }
-    }
-    pub fn add(mut self, field: impl Into<String>, message: impl Into<String>) -> Self {
-        self.errors.entry(field.into()).or_default().push(message.into());
-        self
-    }
+    /// Return `422 Unprocessable Entity`
+    #[error("error in the request body")]
+    UnprocessableEntity {
+        errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+    },
 }
 
 impl Error {
-    pub fn unprocessable_entity(errors: HashMap<String, Vec<String>>) -> Self {
-        Error::UnprocessableEntity(Errors { errors })
+    /// Convenient constructor for `Error::UnprocessableEntity`.
+    pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
+    where
+        K: Into<Cow<'static, str>>,
+        V: Into<Cow<'static, str>>,
+    {
+        let mut error_map = HashMap::new();
+        for (key, val) in errors {
+            error_map.entry(key.into()).or_insert_with(Vec::new).push(val.into());
+        }
+        Self::UnprocessableEntity { errors: error_map }
     }
 
-    pub fn status_code(&self) -> StatusCode {
+    fn status_code(&self) -> StatusCode {
         match self {
-            Error::Unauthorized => StatusCode::UNAUTHORIZED,
-            Error::Forbidden => StatusCode::FORBIDDEN,
-            Error::NotFound => StatusCode::NOT_FOUND,
-            Error::UnprocessableEntity(_) => StatusCode::UNPROCESSABLE_ENTITY,
-            Error::Sqlx(_) | Error::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::Unauthorized => StatusCode::UNAUTHORIZED,
+            Self::Forbidden => StatusCode::FORBIDDEN,
+            Self::NotFound => StatusCode::NOT_FOUND,
+            Self::UnprocessableEntity { .. } => StatusCode::UNPROCESSABLE_ENTITY,
         }
     }
 }
 
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
-        let status = self.status_code();
         match self {
-            Error::UnprocessableEntity(errors) => {
-                (status, Json(errors)).into_response()
-            }
-            Error::Unauthorized => {
-                let mut headers = HeaderMap::new();
-                headers.insert(header::WWW_AUTHENTICATE, "Bearer".parse().unwrap());
-                (status, headers, self.to_string()).into_response()
-            }
-            Error::Sqlx(ref err) => {
-                log::error!("SQLx error: {err:?}");
-                (status, "Internal server error").into_response()
-            }
-            Error::Anyhow(ref err) => {
-                log::error!("Anyhow error: {err:?}");
-                (status, "Internal server error").into_response()
-            }
-            _ => (status, self.to_string()).into_response(),
-        }
-    }
-}
-
-pub trait ResultExt<T> {
-    fn on_constraint<F>(self, constraint: &str, f: F) -> Self
-    where
-        F: FnOnce(&sqlx::Error) -> Error;
-}
-
-impl<T> ResultExt<T> for Result<T, Error> {
-    fn on_constraint<F>(self, constraint: &str, f: F) -> Self
-    where
-        F: FnOnce(&sqlx::Error) -> Error,
-    {
-        self.map_err(|e| {
-            if let Error::Sqlx(ref sqlx_err) = e {
-                if let sqlx::Error::Database(db_err) = sqlx_err {
-                    if let Some(db_constraint) = db_err.constraint() {
-                        if db_constraint == constraint {
-                            return f(sqlx_err);
-                        }
-                    }
+            Self::UnprocessableEntity { errors } => {
+                #[derive(serde::Serialize)]
+                struct Errors {
+                    errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
                 }
+                (StatusCode::UNPROCESSABLE_ENTITY, Json(Errors { errors })).into_response()
             }
-            e
-        })
+            Self::Unauthorized => {
+                (
+                    self.status_code(),
+                    [(WWW_AUTHENTICATE, "Token")],
+                    self.to_string(),
+                ).into_response()
+            }
+            _ => (self.status_code(), self.to_string()).into_response(),
+        }
     }
 }
