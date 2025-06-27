fd139a4e5cb0b25a21fb531adf451ae39530faf6
Akshola00 <akinniyishola07@gmail.com>
2025-06-27 18:27:02 +0100

    feat: Add telemetry setup and enhance logging capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use fortichain_server::{Configuration, http};
+use fortichain_server::{http, telemetry, Configuration};
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+
+    telemetry::setup_tracing();
+
+    tracing::debug!("Initializing configuration");
+    
+    let config = Configuration::new();
+
+    tracing::debug!("Initializing DB pool");
+
+    tracing::debug!("Running Migrations");
+
+    tracing::info!("Starting server on {}", config.listen_address);
+
     let configuration = Configuration::new();
     http::serve(configuration)
         .await
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,1 +1,32 @@
+use tower_http::{
+    classify::{ServerErrorsAsFailures, SharedClassifier},
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+};
+use tracing::Level;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
+// Setup Logger
+pub fn setup_tracing() {
+    // Default to debug if no parameter is specified in the environment.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| "debug".into());
+
+    // Format logs in a readable format with newlines.
+    let formatting_layer = fmt::layer()
+        .with_ansi(true)
+        .with_target(true)
+        .with_level(true)
+        .pretty();
+
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(formatting_layer)
+        .init();
+}
+
+// Logging Middleware
+pub fn trace_layer() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
+    TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+        .on_request(DefaultOnRequest::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO))
+}
