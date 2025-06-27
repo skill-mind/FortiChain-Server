be2c5b166777c116262be45fd07e740bad0c27e2
Akshola00 <akinniyishola07@gmail.com>
2025-06-27 18:17:17 +0100

    Revert "feat: Enhance tracing and telemetry integration in main application"
    This reverts commit 49b820f32f6a034dd5fc48259fe09847e00b35a7.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-use opentelemetry::{global, trace::TracerProvider};
-use opentelemetry_sdk::trace::SdkTracerProvider;
-use opentelemetry_stdout as stdout;
-use tracing_subscriber::{prelude::*, EnvFilter};
 use fortichain_server::{Configuration, http};
-use opentelemetry_appender_tracing::layer;
-
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
-
-    let exporter = opentelemetry_stdout::LogExporter::default();
-
-    
-    let exporter = stdout::SpanExporter::default();
-    let provider = SdkTracerProvider::builder()
-        .with_simple_exporter(exporter)
-        .build();
-        
     let configuration = Configuration::new();
-
-    let filter_otel = EnvFilter::new("info")
-        .add_directive("hyper=off".parse().unwrap())
-        .add_directive("tonic=off".parse().unwrap())
-        .add_directive("h2=off".parse().unwrap())
-        .add_directive("reqwest=off".parse().unwrap());
-    let otel_layer = layer::OpenTelemetryTracingBridge::new(&provider).with_filter(filter_otel);
-    
-    let server_result = http::serve(configuration).await;
-    
-    provider.shutdown().expect("TracerProvider should shutdown successfully");
-    server_result.expect("Failed to start server.");
+    http::serve(configuration)
+        .await
+        .expect("Failed to start server.");
 }
