af9893e2b940bfb80436f065e73b64471cf02744
Yunus Abdulmajid <yunusabdulmajidyunus38@gmail.com>
2025-06-30 12:53:24 +0100

    feat: integrate SQLx for PostgreSQL database support (#56)
    * feat: integrate SQLx for PostgreSQL database support
    
    * fix: clean up formatting in Cargo.toml and Cargo.lock
    
    * fix: clippy warring error

--- src/db.rs
+++ src/db.rs
@@ -1,1 +1,28 @@
+use crate::Configuration;
+use sqlx::{
+    Error,
+    postgres::{PgPool, PgPoolOptions},
+};
 
+// Wrapper type to hold the DB pool.
+#[derive(Clone)]
+pub struct Db {
+    pub pool: PgPool,
+}
+
+impl Db {
+    // Initialize a DB connection and return the Pool.
+    pub async fn new(config: &Configuration) -> Result<Self, Error> {
+        let pool = PgPoolOptions::new()
+            .max_connections(config.max_db_connections)
+            .connect(&config.database_url)
+            .await?;
+        Ok(Db { pool })
+    }
+
+    // executes a simple SELECT 1 query to verify database connectivity
+    pub async fn ping_db(pool: &PgPool) -> Result<(), Error> {
+        sqlx::query("SELECT 1").execute(pool).await?;
+        Ok(())
+    }
+}
--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,23 +1,22 @@
 use std::sync::Arc;
 
+use crate::Config;
+use crate::{Configuration, db::Db};
 use anyhow::Context;
 use axum::Router;
 use tokio::{net::TcpListener, signal};
-
-use crate::Configuration;
 
 mod health_check;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub configuration: Arc<Configuration>,
+    pub db: Db,
+    pub configuration: Config,
 }
 
-pub async fn serve(configuration: Configuration) -> anyhow::Result<()> {
+pub async fn serve(configuration: Arc<Configuration>, db: Db) -> anyhow::Result<()> {
     let addr = configuration.listen_address;
-    let app_state = AppState {
-        configuration: Arc::new(configuration),
-    };
+    let app_state = AppState { configuration, db };
 
     let app = api_router(app_state);
 
