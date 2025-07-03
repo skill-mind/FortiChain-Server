75e5de8147f289cedbc037f1019a26f7ad0cfd5e
shamoo53 <aliyushamsu16@gmail.com>
2025-07-03 13:53:08 +0100

    implementation of Centralized Error Handling and Propagation (#53)
    * implementation of Centralized Error Handling and Propagation
    
    * corrections
    
    * fixed ci/cd errors
    
    * fixed lint errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 pub mod config;
 pub mod db;
+pub mod error;
 pub mod http;
 pub mod middleware;
 pub mod telemetry;
 
 pub use config::*;
+pub use error::*;
 pub use http::*;
+// pub use db::*;
+// pub use middleware::*;
+// pub use telemetry::*;
